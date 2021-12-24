569c9431de2a650d6d58ad58b64df7b35d236931
Krishna Sivakumar <krishnasivaprogrammer@gmail.com>
2021-12-24 14:45:49 +0530

    added Google OAuth2 authentication

--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,121 @@
-const express = require("express");
+import * as express from "express";
+import * as cookieParser from "cookie-parser"
+import * as config from "./config";
+import * as sqlite3 from "sqlite3";
+import * as sqlite from "sqlite";
+import { OAuth2Client } from "google-auth-library"
+import { google } from "googleapis";
 const cors = require("cors");
-const config = require("./config");
-const sqlite3 = require("sqlite3");
-const sqlite = require("sqlite");
-
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oidc');
 
 let app = express();
 app.use(cors());
 app.set("view engine", "ejs");
+app.use(cookieParser())
 app.use(express.static("./public"));
 
-/*
-passport.use(new GoogleStrategy({
-    clientID: config.GOOGLE_CLIENT_ID,
-    clientSecret: config.GOOGLE_CLIENT_SECRET,
-    callbackURL: `${config.SERVER_ROOT_URI}/${config.redirectURI}`
-}, async function verify(issuer, profile) {
-    let db = await sqlite.open({
-        filename: "database.db",
-        driver: sqlite3.Database
-    });
-
-    console.log(profile);
-
-    db.close();
-}));
-*/
-
-let db = sqlite.open({
+let dbPormise = sqlite.open({
     filename: "./database.db",
-
     driver: sqlite3.Database
 })
 
-async function queryRooms(db, query) {
-    let rows = [];
+const authenticatedClient: OAuth2Client = new OAuth2Client(
+    config.GOOGLE_CLIENT_ID,
+    config.GOOGLE_CLIENT_SECRET,
+    config.redirectURIs[0]
+);
 
+async function queryRooms(db: sqlite.Database, query): Promise<any[]> {
     const ac = escape(query.ac);
     const size = escape(query.size);
     const block = escape(query.block);
     const room_no = `%${escape(query.room_no)}%`;
 
-    rows = await db.all("select size, room_no, email from rooms where room_no like ? or size=?", room_no, size);
-
-    return rows;
+    return db.all("select size, room_no, email from rooms where room_no like ? and size=?", room_no, size);
 }
 
-app.get('/', async (req, res) => {
-    db = await db;
+app.get('/', async function home(req, res) {
+    let db = await dbPormise;
+
+    const authURL = authenticatedClient.generateAuthUrl({
+        access_type: "offline",
+        scope: "https://www.googleapis.com/auth/userinfo.profile",
+        hd: "vitstudent.ac.in"                                      // Only allow accounts from the domain 'vitstudent.ac.in'
+    });
+
+    let loggedIn: boolean = false;
+    let name: string = "";
+
+    if (req.cookies.AT != undefined) {
+        // Log the user in if the access token cookie is present
+        loggedIn = true;
+        name = req.cookies.NM;
+        try {
+            // Verify if access token is still valid
+            let getter = google.oauth2({ auth: authenticatedClient, version: "v2" })
+            let _ = await getter.userinfo.get({});
+        } catch (e) {
+            // Log user out if it isn't
+            res.clearCookie("NM");
+            res.clearCookie("AT");
+            loggedIn = false;
+            console.error(e);
+        }
+    }
 
     res.render("index", {
-        authLink: "/login",
+        user: {
+            loggedIn: loggedIn,
+            name: name
+        },
+        authLink: authURL,
         rows: await queryRooms(db, req.query),
         size: req.query.size,
         ac: req.query.ac
     });
 });
 
-// app.get('/login', passport.authenticate("google"));
+app.get("/auth/google", async function login(req, res) {
+    /*
+        Logs the user in and sets the following cookies:
+        NM: Google user's given_name
+        AT: Access Token
+        RF: Refresh Token
+    */
+
+    const authCode = req.query.code as string;
+    const r = await authenticatedClient.getToken(authCode);
+    authenticatedClient.setCredentials(r.tokens);
+
+    // Get userinfo from googleapis
+    let getter = google.oauth2({ auth: authenticatedClient, version: "v2" })
+    let response = await getter.userinfo.get({});
+    console.log(authenticatedClient.credentials);
+
+    // Set relevant cookies
+    res.cookie("NM", response.data.given_name, { httpOnly: true });
+    res.cookie("AT", authenticatedClient.credentials.access_token, { httpOnly: true });
+    res.cookie("RF", authenticatedClient.credentials.refresh_token, { httpOnly: true });
+
+    // Log the login event
+    console.info(`[INFO] ${response.data.email} logged in at ${new Date()}`)
+
+    res.redirect("/");
+});
+
+app.get("/logout", async function logout(req, res) {
+    /*
+        Logs the user out and clears all cookies
+    */
+
+    // Clear user-related cookies
+    res.clearCookie("NM");
+    res.clearCookie("AT");
+    res.clearCookie("RF");
+
+    // Log the logout event
+    console.info(`[INFO] ${req.cookies.NM} logged out at ${new Date()}`)
+    res.redirect("/");
+})
 
 let PORT = process.env.PORT || 4000;
 
