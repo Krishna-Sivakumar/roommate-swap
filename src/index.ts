8214d5610438bf1251887e324bf35f766347413a
Krishna Sivakumar <krishnasivaprogrammer@gmail.com>
2021-12-29 03:53:04 +0530

    swapped cookies with server-side sessions; removed queryRooms

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,28 @@
 import * as express from "express";
 import * as cookieParser from "cookie-parser";
+import * as cors from "cors";
+import * as session from "express-session";
 import * as formidable from "formidable";
 import * as config from "./config";
 import * as sqlite3 from "sqlite3";
 import * as sqlite from "sqlite";
 import { OAuth2Client } from "google-auth-library"
 import { google } from "googleapis";
-const cors = require("cors");
+
+const SQLiteStore = require("connect-sqlite3")(session);
 
 import * as auth from "./auth";
 
 let app = express();
 app.use(cors());
 app.set("view engine", "ejs");
-app.use(cookieParser())
+app.use(cookieParser());
+app.use(session({
+    secret: "secret2",
+    store: new SQLiteStore,
+    saveUninitialized: false,
+    resave: false
+}));
 app.use(express.static("./public"));
 
 let dbPromise = sqlite.open({
@@ -39,15 +48,6 @@ async function ensureDB(uri: string) {
     db.exec("CREATE TABLE IF NOT EXISTS rooms(email TEXT PRIMARY KEY, room_no INTEGER, size INTEGER, ac INTEGER, swap INT DEFAULT 0);");
 }
 
-async function queryRooms(db: sqlite.Database, query): Promise<any[]> {
-    const ac = escape(query.ac);
-    const size = escape(query.size);
-    const block = escape(query.block);
-    const room_no = `%${escape(query.room_no)}%`;
-
-    return db.all("select size, room_no, email from rooms where room_no like ? and size=?", room_no, size);
-}
-
 async function firstLogin(db: sqlite.Database, email: string): Promise<Boolean> {
     let row = await db.get("SELECT size, ac FROM rooms WHERE email = ?", email);
     return !(row != undefined && row.size != null && row.ac != null);
@@ -55,7 +55,7 @@ async function firstLogin(db: sqlite.Database, email: string): Promise<Boolean> 
 
 async function setSwap(db: sqlite.Database, email: string, checked: string) {
     let swap = (checked == "on") ? 1 : 0;
-    await db.run("UPDATE TABLE rooms SET swap = ? WHERE email = ?", swap, email);
+    await db.run("UPDATE rooms SET swap = ? WHERE email = ?", swap, email);
 }
 
 async function setRoom(db: sqlite.Database, email: string, room_no: number) {
@@ -108,45 +108,35 @@ app.get('/', async function home(req, res) {
     let name: string = "";
     let user: Object = { loggedIn: false }
 
-    if (req.cookies.AT != undefined) {
-        const isValid = await auth.tokenIsValid(req.cookies.AT);
+    if (req.session["AT"] != undefined) {
+        const isValid = await auth.tokenIsValid(req.session["AT"]);
         if (isValid) {
             user = {
                 loggedIn: true,
-                name: req.cookies.NM,
-                firstLogin: await firstLogin(db, req.cookies.EM)
+                name: req.session["NM"],
+                firstLogin: await firstLogin(db, req.session["EM"])
             };
-        } else if (req.cookies.RF != "undefined") {
+        } else if (req.session["RF"] != "undefined") {
             // Check if refresh token is present, and use it to refresh the access token
             try {
-                const access_token = await auth.refreshToken(config.GOOGLE_CLIENT_ID, config.GOOGLE_CLIENT_SECRET, req.cookies.RF);
-                req.cookies("AT", access_token, { httpOnly: true });
+                const access_token = await auth.refreshToken(config.GOOGLE_CLIENT_ID, config.GOOGLE_CLIENT_SECRET, req.session["RF"]);
+                req.session["AT"] = access_token;
             } catch (e) {
-                // User isn't logged in, clear cookies if present
-                res.clearCookie("EM");
-                res.clearCookie("NM");
-                res.clearCookie("AT");
-                res.clearCookie("RF");
+                // User isn't logged in, clear the session
+                req.session.destroy(() => { });
             }
         } else {
-            // User isn't logged in, clear cookies if present
-            res.clearCookie("EM");
-            res.clearCookie("NM");
-            res.clearCookie("AT");
-            res.clearCookie("RF");
+            // User isn't logged in, clear the session
+            req.session.destroy(() => { });
         }
     } else {
-        // User isn't logged in, clear cookies if present
-        res.clearCookie("EM");
-        res.clearCookie("NM");
-        res.clearCookie("AT");
-        res.clearCookie("RF");
+        // User isn't logged in, clear the session
+        req.session.destroy(() => { });
     }
 
     res.render("index", {
         user: user,
         authLink: authURL,
-        rows: await queryRooms(db, req.query),
         ava: await getAvailableRooms(db),
         size: req.query.size,
         ac: req.query.ac
@@ -164,7 +154,7 @@ app.post("/form/init", async function initialLogin(req, res) {
         "UPDATE rooms SET size = ?, ac = ? WHERE email = ?;",
         formData.fields["initial-size"],
         ac,
-        req.cookies.EM
+        req.session["EM"]
     );
 
     res.redirect("/");
@@ -175,15 +165,15 @@ app.post("/form/swap", async function swap(req, res) {
 
     const formData = await parseForm(req);
 
-    setSwap(db, req.cookies.EM, formData.fields["swap"]);
-    setRoom(db, req.cookies.EM, formData.fields["room-no"]);
+    setSwap(db, req.session["EM"], formData.fields["swap"]);
+    setRoom(db, req.session["EM"], formData.fields["room-no"]);
 
     res.redirect("/");
 })
 
 app.get("/auth/google", async function login(req, res) {
     /*
-        Logs the user in and sets the following cookies:
+        Logs the user in and sets the following session variables:
         NM: Google user's given_name
         AT: Access Token
         RF: Refresh Token
@@ -197,11 +187,11 @@ app.get("/auth/google", async function login(req, res) {
     let getter = google.oauth2({ auth: authenticatedClient, version: "v2" })
     let response = await getter.userinfo.get({});
 
-    // Set relevant cookies
-    res.cookie("EM", response.data.email, { httpOnly: true });
-    res.cookie("NM", response.data.given_name, { httpOnly: true });
-    res.cookie("AT", authenticatedClient.credentials.access_token, { httpOnly: true });
-    res.cookie("RF", authenticatedClient.credentials.refresh_token, { httpOnly: true });
+    // Setting session variables
+    req.session["EM"] = response.data.email;
+    req.session["NM"] = response.data.given_name;
+    req.session["AT"] = authenticatedClient.credentials.access_token;
+    req.session["RF"] = authenticatedClient.credentials.refresh_token;
 
     // Instantiate a new record with no values set in the database, if the email is logging in for the first time
     let db = await dbPromise;
@@ -215,17 +205,15 @@ app.get("/auth/google", async function login(req, res) {
 
 app.get("/logout", async function logout(req, res) {
     /*
-        Logs the user out and clears all cookies
+        Logs the user out and clears the session
     */
 
-    // Clear user-related cookies
-    res.clearCookie("EM");
-    res.clearCookie("NM");
-    res.clearCookie("AT");
-    res.clearCookie("RF");
-
     // Log the logout event
-    console.info(`[INFO] ${req.cookies.EM} logged out at ${new Date()}`)
+    console.info(`[INFO] ${req.session["EM"]} logged out at ${new Date()}`)
+
+    // Clear the session
+    req.session.destroy(() => { });
+
     res.redirect("/");
 })
 
