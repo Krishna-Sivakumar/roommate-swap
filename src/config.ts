c4e3cc4cb1211248b1e47e08f34603f5fa553953
Siddharth Singh <simsid66@gmail.com>
2021-12-29 12:44:36 +0530

    make oauth id and secret env vars

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,9 @@
-export const GOOGLE_CLIENT_ID = "your-client-id";
-export const GOOGLE_CLIENT_SECRET = "your-client-secret";
+if (!process.env.VSWAP_CLIENT_ID || !process.env.VSWAP_CLIENT_SECRET || !process.env.VSWAP_SESSION_SECRET) {
+    throw new Error("You must set the VSWAP_CLIENT_ID, VSWAP_SESSION_SECRET, and VSWAP_CLIENT_SECRET environment variables to run VSwap.");
+}
+export const SESSION_SECRET = process.env.VSWAP_SESSION_SECRET;
+export const GOOGLE_CLIENT_ID: string = process.env.VSWAP_CLIENT_ID;
+export const GOOGLE_CLIENT_SECRET: string = process.env.VSWAP_CLIENT_SECRET;
 export const redirectURIs = [
     "http://localhost:4000/auth/google"
 ];
