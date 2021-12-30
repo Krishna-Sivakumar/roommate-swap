f3e641d8caf60b1254a5523a2f55e4e6e0017009
Siddharth Singh <simsid66@gmail.com>
2021-12-31 05:09:12 +0530

    switch to eta for templating

--- src/deps.ts
+++ src/deps.ts
@@ -2,7 +2,7 @@ export { opine, serveStatic, urlencoded, json } from "https://deno.land/x/opine@
 export type { Opine, OpineRequest, OpineResponse, NextFunction } from "https://deno.land/x/opine@2.0.0/mod.ts";
 export { DB } from "https://deno.land/x/sqlite@v3.2.0/mod.ts";
 export type { Row } from "https://deno.land/x/sqlite@v3.2.0/mod.ts";
-export { renderFileToString } from "https://deno.land/x/dejs@0.10.2/mod.ts";
+export * as eta from "https://deno.land/x/eta@v1.12.3/mod.ts";
 export { getCookies } from "https://deno.land/std@0.116.0/http/cookie.ts";
 export { v4 } from "https://deno.land/std@0.116.0/uuid/mod.ts";
 export { opineCors } from "https://deno.land/x/cors/mod.ts";
