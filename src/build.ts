1fa3385077b470aa76dde960b3ad9559657bc0c1
Siddharth Singh <simsid66@gmail.com>
2021-12-31 06:08:01 +0530

    remove old build files

--- src/build.ts
+++
@@ -1,7 +0,0 @@
-import { Leaf } from "https://deno.land/x/leaf@v1.0.4/mod.ts";
-
-Leaf.compile({
-    modulePath: "./main.ts",
-    contentFolders: ["./embed-views", "./embed-public"],
-    output: 'roomswap'
-});
